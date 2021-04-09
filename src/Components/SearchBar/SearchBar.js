fe0484ad418f16d177e115a31f7eb98c48edec3b
Rebecca Meyer <faustorebeccabsit@gmail.com>
2021-04-09 11:37:25 +0200

    removed search image onfocus

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,9 +1,15 @@
-const SearchBar = ( {onSearchChange}) => {
+import { useRef } from 'react';
 
+const SearchBar = ( {onSearchChange}) => {
+    const inputFocus = useRef();
+
+    const hideSearchImage = () => {
+        inputFocus.current.style.display = "none";
+    }
     return(
         <div className="search-bar">
-            <input  onChange={onSearchChange} type="text" id="search-bar__text-field" className="pa3" placeholder="Search" />
-            <i className="fas fa-search o-50"></i>
+            <input  onFocus={hideSearchImage} onChange={onSearchChange} type="text" id="search-bar__text-field" className="pa3" placeholder="Search" />
+            <i ref={inputFocus} className="fas fa-search o-50"></i>
         </div>
     );
 }
